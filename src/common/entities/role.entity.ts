cfa40a0b7c37adda5e8c6475e2a0f614e24070a6
Nguyen Khoi <khoibene@gmail.com>
2025-10-07 00:16:07 +0700

    add user-role

--- src/common/entities/role.entity.ts
+++ src/common/entities/role.entity.ts
@@ -1,9 +1,11 @@
 import { DateTimeEntity } from './base/dateTimeEntity';
 import {
   Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany,
+  ManyToMany,
 } from 'typeorm';
 import { RoleScope } from './enums';
 import { RolePermission } from './role-permission.entity';
+import { User } from './user.entity';
 
 @Entity('roles')
 @Unique(['scope', 'name'])
@@ -22,4 +24,7 @@ export class Role extends DateTimeEntity {
 
   @OneToMany(() => RolePermission, (rp) => rp.role)
   rolePermissions!: RolePermission[];
+
+  @ManyToMany(() => User, (user) => user.roles)
+  users!: User[];
 }
