5cff7ccc5aeda3caa38433fb716626f8ec052112
Nguyen Khoi <khoibene@gmail.com>
2025-11-09 16:26:37 +0700

    feat: add handle service response and validateReq middleware

---
+++ src/utils/http-handler.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ZodError, ZodSchema } from 'zod';
+
+import { ResponseStatus, ServiceResponse } from '../provides/service.response';
+
+// Format and send service response
+export const handleServiceResponse = (serviceResponse: ServiceResponse<any>, response: Response) => {
+  return response.status(serviceResponse.statusCode).send(serviceResponse);
+};
+
+// Middleware to validate request using Zod schema
+export const validateRequest = (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+  try {
+    schema.parse({ body: req.body, query: req.query, params: req.params });
+    next();
+  } catch (err) {
+    const errorMessage = `Invalid input: ${(err as ZodError).issues.map((e) => e.message).join(', ')}`;
+    const statusCode = StatusCodes.BAD_REQUEST;
+    res.status(statusCode).send(new ServiceResponse<null>(ResponseStatus.Failed, errorMessage, null, statusCode));
+  }
+};
